2d4a23877e0babda70605b65fa9e092ccbdf8cc0
e501843 <joel.aebi@sbb.ch>
2021-05-21 16:42:40 +0200

    auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from "./core/services/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hornussergesellschaft-urtenen';
+
+  constructor(private authService: AuthService) {
+    this.authService.login('hornusserurtenen@gmail.com', 'shotmisti').then(res => {
+      console.log(res.user?.uid)
+    });
+  }
 }
