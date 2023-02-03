a2ea55ae978224bdea4364753510b67eb05b4b94
Joel Aebi <joelaebi1@gmail.com>
2023-02-03 16:46:33 +0100

    filter last results for current year

--- src/app/shared/services/game.service.ts
+++ src/app/shared/services/game.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable} from "rxjs";
+import {flatMap, Observable} from "rxjs";
 import { Game } from "../models/game.model";
 import {filter, map} from "rxjs/operators";
 import {fromPromise} from "rxjs/internal/observable/innerFrom";
@@ -22,13 +22,16 @@ export class GameService {
   }
 
   getLatestGames(): Observable<Game[]> {
+    let currentYearDate = new Date(new Date().getFullYear(), 0, 1);
     return this.firestore.collection<Game>('game', ref => ref.orderBy('createdAt', 'desc').limit(5)).get().pipe(
       map(docs => {
         let games: Game[] = [];
         docs.forEach(doc => {
           let game = doc.data() as Game;
-          game.id = doc.id;
-          games.push(game);
+          if (new Date(game.createdAt.seconds * 1000) > currentYearDate) {
+            game.id = doc.id;
+            games.push(game);
+          }
         })
         return games;
       })
@@ -37,6 +40,7 @@ export class GameService {
 
   getGame(documentId: string): Observable<Game | undefined> {
     let currentYearDate = new Date(new Date().getFullYear(), 0, 1);
+
     return this.firestore.collection<Game>('game').doc(documentId).get().pipe(
       filter(res => {
         return new Date(res.data().createdAt.seconds * 1000) > currentYearDate
